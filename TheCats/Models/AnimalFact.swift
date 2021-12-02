fad61a80da22d7a8a6a2d99266b8b719cb624189
urszulawiertel <urszulawiertel@gmail.com>
2021-12-02 23:10:01 +0100

    Update the facts model, add CodingKeys for the 'id' property

--- TheCats/Models/AnimalFact.swift
+++ TheCats/Models/AnimalFact.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 struct AnimalFact: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case status
+        case text
+        case type
+        case createdAt
+    }
+    let id: String
+    let status: Status
     let text: String
     let type: String
     let createdAt: String?
+}
+
+struct Status: Codable {
+    enum CodingKeys: String, CodingKey {
+        case verified
+    }
+    let verified: Bool?
 }
