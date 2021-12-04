5a199aa743ec560c3f5cfe0bccbf11ad2b8b977c
urszulawiertel <urszulawiertel@gmail.com>
2021-12-04 22:02:41 +0100

    Update UI and cells

--- TheCats/AnimalFacts/AnimalFactsTableViewCell.swift
+++ TheCats/AnimalFacts/AnimalFactsTableViewCell.swift
@@ -10,9 +10,13 @@ import UIKit
 class AnimalFactsTableViewCell: UITableViewCell {
     @IBOutlet private weak var prefixLabel: UILabel!
     @IBOutlet private weak var contentLabel: UILabel!
+    @IBOutlet private weak var typeLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
 
     func configureCell(for model: AnimalFact?, dateFormatter: DateConverting) {
         prefixLabel.text = "Fact #\(model?.index ?? 1):"
         contentLabel.text = model?.text
+        typeLabel.text = model?.type
+        dateLabel.text = dateFormatter.formatDate(model?.createdAt ?? "")
     }
 }
